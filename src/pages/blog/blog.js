45a92a07de9e4064bade9c02e2edf2baa856a068
chris-ecklund <christopher.ecklund.eng@gmail.com>
2023-02-22 16:02:17 -0500

    -new commit

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import Seo from "../components/seo";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
 
 export const query = graphql`
   query {
@@ -64,3 +64,7 @@ const BlogPage = ({ data }) => {
 export const Head = () => <Seo title="My Blog Posts" />;
 
 export default BlogPage;
+
+//friday, pick up here
+//1. Start by using GraphiQL to create a page query for your blog post page template
+// 3/4 way down the part6 page
