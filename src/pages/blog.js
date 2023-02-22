b81cb1107dee36970fc2851a4179fa6fce431cd2
chris-ecklund <christopher.ecklund.eng@gmail.com>
2023-02-22 09:58:08 -0500

    -add query to BLOG page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,6 +5,11 @@ import Seo from '../components/seo'
 
 export const query = graphql`
   query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     allFile {
       nodes {
         name
@@ -13,7 +18,12 @@ export const query = graphql`
   }
 `
 
-const BlogPage = () => {
+//console.log("the query returns: ", {data.siteMetadata});
+
+const BlogPage = (data) => {
+
+  console.log("the query returns: ", {data});
+
   return (
     <Layout pageTitle="My Blog Posts">
       <p>My cool posts will go in here</p>
